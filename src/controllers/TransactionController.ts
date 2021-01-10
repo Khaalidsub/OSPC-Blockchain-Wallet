54ab3c832a517549b5edb391cab232e180582294
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-11 00:58:46 +0800

    transaction controller added

---
+++ src/controllers/TransactionController.ts
@@ -0,0 +1,26 @@
+import { Controller } from '@nestjs/common/decorators/core';
+import {
+  Body,
+  Post,
+  Get,
+  Delete,
+  Put,
+  Param,
+} from '@nestjs/common/decorators/http';
+import { BlockChain } from 'src/models/Blockchain';
+import { KeyMaker } from 'src/models/KeyMaker';
+import { Transaction } from 'src/models/Transaction';
+
+@Controller('blockchain')
+export class TransactionController {
+  constructor(private blockchain: BlockChain, private keymaker: KeyMaker) {}
+  @Post()
+  addTransaction(transaction: Transaction, signature: string) {
+    this.keymaker.verifySignature(transaction, signature);
+  }
+  //   broadCastTransaction() {}
+  @Get(':id')
+  getTransaction(transactionId: string) {
+    return this.blockchain.getTransaction(transactionId);
+  }
+}
