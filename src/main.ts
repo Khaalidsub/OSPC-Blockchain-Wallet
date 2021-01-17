16d3630d5e0ea87da37a8649ed3ed5f5c5cc3540
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-17 21:05:11 +0800

    connecting around nginx...1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
-
+import * as cookieParser from 'cookie-parser';
+import { AllExceptionsFilter } from './exceptions.filter';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.set('trust proxy', 1);
+  // app.enableCors({ origin: 'http://127.0.0.1:3050' });
+  // app
+  //   .use(cookieParser())
+  //   .use(bodyParser.json())
+  //   .use(
+  //     bodyParser.urlencoded({
+  //       extended: true,
+  //     }),
+  //   );
+
   await app.listen(3000);
 }
 bootstrap();
