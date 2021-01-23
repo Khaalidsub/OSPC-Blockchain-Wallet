bcd1d5afdff9f83e95157d65edf06432e07acf2f
khaalid subaan <khaalidsubaan@gmail.com>
2021-01-23 16:41:15 +0800

    endpoints for demo functions

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, HttpService, Post, Render } from '@nestjs/common';
 import { BlockChain } from 'src/models/Blockchain';
 
 @Controller()
 export class AppController {
-  constructor(private readonly blockchain: BlockChain) {}
+  constructor(
+    private readonly blockchain: BlockChain,
+    private httpService: HttpService,
+  ) {}
 
   @Get()
   @Render('index')
-  getHello() {
+  getHome() {
     const chains = this.blockchain.chain;
     const nodes = this.blockchain.networkNodes;
     const pendingTransactions = this.blockchain.pendingTransactions;
@@ -15,5 +18,38 @@ export class AppController {
       this.blockchain.currentNodeUrl.lastIndexOf('/') + 1,
     );
     return { node, chains, nodes, pendingTransactions };
+  }
+  @Post('create-transaction')
+  @Render('index')
+  async createTransaction() {
+    await this.httpService
+      .post(`${this.blockchain.currentNodeUrl}/transaction/add`, {
+        transaction: {
+          sender: 'khaalid',
+          recipient: 'ibrahim',
+          transactionType: 'topup',
+          data: { charge: '15.0' },
+        },
+      })
+      .toPromise();
+    return this.getHome();
+  }
+
+  @Post('create-block')
+  @Render('index')
+  async createBlock() {
+    await this.httpService
+      .get(`${this.blockchain.currentNodeUrl}/blockchain/mine`)
+      .toPromise();
+    return this.getHome();
+  }
+
+  @Get('consensus')
+  @Render('index')
+  async consensus() {
+    await this.httpService
+      .get(`${this.blockchain.currentNodeUrl}/blockchain/consensus`)
+      .toPromise();
+    return this.getHome();
   }
 }
